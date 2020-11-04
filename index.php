e5a758cad7b869b1a97dec0c06d81058d15af63d
advenrihimone <advenrihimone@gmail.com>
2020-11-04 09:01:32 +0800

    background
    menambahkan background

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<<<<<<< HEAD
+    <link rel="stylesheet" href="src/css/index.css">
+=======
+>>>>>>> 165b855ec13f03c351995dc9c860044b99cb90dc
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<<<<<<< HEAD
+    <div class="bg">
+        <div class="bgman">
+            <img src="image/indeximg/bgman.jpg" alt="">
+        </div>
+        <div class="bgwoman">
+            <img src="image/indeximg/bgwoman.jpg" alt="">
+        </div>
+    </div>        
+=======
+    <img src="" alt="">
+>>>>>>> 165b855ec13f03c351995dc9c860044b99cb90dc
+</body>
+</html>
